f5c474b9764b66d678ead5e7339a8a9933016094
Princess Sampson <ps@kingonomics.com>
2016-08-11 10:17:08 -0400

    Initial Commit

---
+++ Enum/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Enum
+//
+//  Created by Princess Sampson on 8/11/16.
+//  Copyright © 2016 Princess Sampson. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
